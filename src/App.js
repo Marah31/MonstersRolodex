f5fe7e321c466c5ee265b7e36f84e7683c54b016
Marah N <marah.n35@gmail.com>
2025-08-11 20:00:50 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+// rule of thumb, best practice for arrays or data sets in cases like filtering is immunity, which means don't modify the original array if you want to add or remove something, create a new one
 import './App.css';
+import { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+// what is the order for each component or part of the code to run?
+// 1. constructor, this is the first thing that get executed in any and all classes
+// 2. render, this run second as it determin what will be displayed to the user
+// 3. for this code, the third will be componentDidMount() function 
+class App extends Component {
+  constructor(){ // here, this means when the app is one, this is the first thing that get run
+    super();
+    // I want to initiate a state
+    this.state = {
+      monsters: [],
+      searchField: ''
+      
+    };
+  }
+  // I want to fetch the monsters from a server
+  // what I need to think about are three main things:
+  // 1. when do I get the list? here I want it to load as soon as the component is mounted(aka displayed for the first time) 
+  // 2. where do I put the list? 
+  // 3. how do I get the list? 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //used for the 'when' I want the list to be fetched
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').then( (response)=> // notice here I don't have {} after the arrow function
+      response.json()).then((users)=>this.setState(()=>{
+        return {monsters: users}
+      })); // it because not writing it here will make the '.then' function has implicit return, that will returns value without the need to write it
+  }
+
+  onSearchChange = (event)=>{
+    const searchField = event.target.value.toLocaleLowerCase();        
+    this.setState(()=>{
+      return {searchField}; // this will trigger a re-render since the state is changing, thus will keep update the page as long as the user is typing different things 
+      });
+  } 
+  render(){
+
+    // to make it more efficient and clean, let's assigne values instead of using this stat
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster)=>{ // created a new array for immunity
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+        <h1 className='app-title'>Monsters Rodolex</h1>
+        <SearchBox className= 'monsters-search-box' onChangeHandler ={onSearchChange} placeholder = 'search monsters'/>
+        <CardList monsters={filteredMonsters}/> {/* note here that the components I make have to have Capital latter at the beggining*/}
+        
+      </div> 
+    );
+  }
+  
 }
 
 export default App;
